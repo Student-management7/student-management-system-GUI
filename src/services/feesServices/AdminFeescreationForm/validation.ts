681189a5a35dfd579f57fd16083d1394e30afce0
TechWithShivank <136160511+TechWithShivank@users.noreply.github.com>
2025-03-12 09:27:10 +0530

    check some bugs 12/3/25

--- src/services/feesServices/AdminFeescreationForm/validation.ts
+++ src/services/feesServices/AdminFeescreationForm/validation.ts
@@ -2,17 +2,17 @@ import * as Yup from "yup";
 
 export const validationSchema = Yup.object({
   className: Yup.string().required("Class is required"),
-  schoolFee: Yup.number().required("School fee is required ").min(100),
-  sportsFee: Yup.number().required("Sports fee is required and correct format").min(0),
-  bookFee: Yup.number().required("Book fee is required").min(0),
-  transportation: Yup.number().required("Transportation fee is required").min(0),
+  schoolFee: Yup.number().required("School fee is required ").min(100,"School fee must be greater than or equal to 100"),
+  sportsFee: Yup.number().required("Sports fee is required and correct format").min(0,"Sports fee must be greater than or equal to 0"),
+  bookFee: Yup.number().required("Book fee is required").min(0,"Book fee must be greater than or equal to 0"),
+  transportation: Yup.number().required("Transportation fee is required").min(0,"Transportation fee must be greater than or equal to 0"),
   otherAmount: Yup.array().of(
     Yup.object({
       name: Yup.string() .matches(
         /^(?=.*[A-Za-z])[A-Za-z0-9\s.,-]*$/,
-        " must contain at least one letter and can include numbers, spaces, and "
+        " Name must contain at least one letter and can include numbers, spaces, and dot"
       ),
-      amount: Yup.number().min(0, 'Amount must be 0 ot greater then 0'),
+      amount: Yup.number().min(0," Other amount fee must be greater than or equal to 0"),
     })
   ),
 });
