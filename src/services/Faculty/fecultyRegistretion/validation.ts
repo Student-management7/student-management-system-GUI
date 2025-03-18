63322c186c65e25268eb508deea46e3981f252fb
TechWithShivank <136160511+TechWithShivank@users.noreply.github.com>
2025-03-18 13:35:00 +0530

    fix perimisson page key and fix faculty form address issue 18/3/25

--- src/services/Faculty/fecultyRegistretion/validation.ts
+++ src/services/Faculty/fecultyRegistretion/validation.ts
@@ -15,7 +15,7 @@ export const facultyValidationSchema = (editmode: boolean) =>
     fact_address: Yup.string().required('Address is required')
     .min(3, "Address must be at least 3 characters")
     .matches(
-      /^(?=.*[A-Za-z])[A-Za-z0-9\s.,-]*$/,
+      /^(?=.*[A-Za-z])[A-Za-z0-9\s.,\-(){}]*$/,
       "Address must contain at least one letter and can include numbers, spaces, and .,-"
     ).max(60, "Name must be at most 20 characters"),
     fact_city: Yup.string().required('City is required').matches(
@@ -33,17 +33,19 @@ export const facultyValidationSchema = (editmode: boolean) =>
 
     // Conditional Validation based on edit mode
     email: Yup.string()
-      .email('Invalid email')
-      .required('Email is required')
-      .when([], {
-        is: () => editmode,
-        then: schema => schema.required('Email is required in edit mode'),
-      }),
+      .email('Invalid email'),
+     
+      // .when([], {
+      //   is: () => editmode,
+      //   then: schema => schema.notRequired(),
+      //   otherwise: (schema) => schema.required('Email is required'),
+      // }),
 
-    fact_password: Yup.string().required('Password is required')
-      .min(6, 'Password must be at least 6 characters')
-      .when([], {
-        is: () => editmode,
-        then: schema => schema.required('Password is required in edit mode'),
-      }),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters'),
+      // .when([], {
+      //   is: () => editmode,
+      //   then: schema => schema.notRequired(),
+      //   otherwise: (schema) => schema.required('Email is required'),
+      // }),
   });
